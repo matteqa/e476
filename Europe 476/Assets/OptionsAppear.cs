f30463dc7c59117058a85b1b152a6bc2c5187675
FLEMING <matthew.fleming.02@education.tas.edu.au>
2016-11-08 10:49:45 +1100

    First commit

---
+++ Europe 476/Assets/OptionsAppear.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsAppear : MonoBehaviour {
+	
+	public void ShowOptions() {
+		Debug.Log ("test");
+		transform.SetAsFirstSibling();
+	}
+}
---
+++ Europe 476/Assets/ScrollingBG.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBG : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > -1710) {
+			transform.position += new Vector3 (-1f, 0f, 0f);
+		} 
+		else {
+			transform.position += new Vector3 (3420, 0f, 0f);
+		}
+	}
+}
